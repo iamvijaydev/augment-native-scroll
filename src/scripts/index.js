473fdca3048002c7f17e8646549c5da83bbad8d9
Vijay Dev <vijaydev.in@gmail.com>
2017-01-11 12:47:07 +0530

    react router and eslint added

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,28 @@
 require("../scss/styles.scss");
 import React from 'react';
 import {render} from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router'
 
 import App from './App';
 
 render(
     <App />,
+    document.getElementById('app')
+);
+
+
+require("../scss/style.scss");
+import React from 'react';
+import {render} from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router'
+
+import Landing from './Landing';
+import Report from './report/Report';
+
+render(
+    <Router history={browserHistory}>
+        <Route path="/" component={Landing}/>
+        <Route path="/report" component={Report}/>
+    </Router>,
     document.getElementById('app')
 );
